95b591ddcfd602ea127ce03ecaeef5624c393dda
gvmossato <gabriel-vmf@hotmail.com>
2021-06-21 13:38:33 -0300

    code rewrite to meet problem requirements

--- qr.py
+++ qr.py
@@ -7,20 +7,20 @@ def sign(x: float) -> float:
     else:
         return -1.0
 
-def wilkinson_shift(alpha: float, alpha_prev: float, beta_prev: float) -> float:
-    d = (alpha_prev - alpha) / 2.0
-    mu = alpha + d - sign(d) * np.sqrt(d**2 + beta_prev**2)
+def wilkinson_shift(alpha: float, beta: float, alpha_last: float) -> float:
+    d = (alpha - alpha_last) / 2.0
+    mu = alpha_last + d - sign(d) * np.sqrt(d**2 + beta**2)
 
     return mu
 
 def cos_and_sin(alpha: float, beta: float) -> "tuple[float, float]":
     if np.abs(alpha) > np.abs(beta):
         tau = - beta / alpha
-        c = 1 / np.sqrt(1 + tau**2)
+        c = 1.0 / np.sqrt(1.0 + tau**2)
         s = c * tau
     else:
         tau = - alpha / beta
-        s = 1 / np.sqrt(1 + tau**2)
+        s = 1.0 / np.sqrt(1.0 + tau**2)
         c = s * tau
 
     return (c, s)
@@ -36,57 +36,51 @@ def givens_matrix(n: int, i: int, j: int, c: float, s: float) -> np.ndarray:
 
     return G
 
-def QR(A0: np.ndarray, epsilon: float=1e-6, shifted: bool=True) -> "tuple[np.ndarray, np.ndarray]":
-    n = A0.shape[0]
-
-    I = np.eye(n)
-    Q = np.copy(I)
-    R = Q @ A0
+def givens_rotations(A):
+    n = A.shape[0]    
+    R = np.copy(A)    
+    Q = np.eye(n)
 
     for m in range(n-1):
-        beta_prev = np.inf # Initialize beta as something big
-        iter_count = 1 # Counts iterations per eigenvalue found
+        alpha = R[m, m]
+        beta = R[m+1, m]
 
-        while np.abs(beta_prev) >= epsilon:
-            alpha = R[m, m]
-            beta = R[m+1, m]
+        i, j = (m, m+1)
+        c, s = cos_and_sin(alpha, beta)
+        G = givens_matrix(n, i, j, c, s)
 
-            i, j = (m, m+1)
-            c, s = cos_and_sin(alpha, beta)
-            G = givens_matrix(n, i, j, c, s)
-            mu = wilkinson_shift(alpha, alpha_prev, beta_prev) if (shifted and iter_count > 1) else 0.0
-
-            R = R - mu*I
-            Q = Q @ G.T
-            R = G @ R + mu*I
-
-            alpha_prev = alpha
-            beta_prev = beta
-
-            iter_count += 1
-        
-        R[m+1, m] = 0 # If while loop stops, beta converged (is zero)
-
-        print(f"Element in positon {(m, m)} converged to eigenvalue after {iter_count} iterations.")
-
-    # Last eigenvalue converges with last but one eigenvalue
-    print(f"Element in positon {(m+1, m+1)} converged to eigenvalue after {iter_count} iterations.")
-
-    #fix_sign = [ sign(x) for x in R[0,:] ]
-    #Q *= fix_sign
-    #R *= fix_sign
+        Q = Q @ G.T
+        R = G @ R
 
     return (Q, R)
 
+def QR(A0: np.ndarray, epsilon: float=1e-16, shifted: bool=True) -> "tuple[np.ndarray, np.ndarray]":
+    n = A0.shape[0]
 
-#A = np.array(
-#    [[4., 3., 0.],
-#     [3., 4., 3.],
-#     [0., 3., 4.]]
-#)
+    I = np.eye(n)
+    A = np.copy(A0)
+    V = np.eye(n)
 
-#Q, R = QR(A)
-#Q_np, R_np = np.linalg.qr(A)
+    k = 0
 
-#print('Self\nQ =\n', Q, '\n', 'R =\n', R)
-#print('Numpy\nQ =\n', Q_np, '\n', 'R =\n', R_np)
+    for m in range(n-1, 0, -1):       
+        beta = A[m, m-1]
+
+        while np.abs(beta) >= epsilon:
+
+            mu = wilkinson_shift(alpha, beta, alpha_last) if (shifted and k > 0) else 0.0
+
+            A = A - mu*I
+            Q, R = givens_rotations(A)
+
+            A = R @ Q + mu*I
+            V = V @ Q
+
+            beta = A[m, m-1]
+
+            alpha = A[m-1, m-1]
+            alpha_last = A[m, m]
+
+            k += 1
+
+    return (V, A, k)
