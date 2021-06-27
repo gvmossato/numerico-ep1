545dc00a453a3564f26f55f1b31f4882f02fb84d
gvmossato <gabriel-vmf@hotmail.com>
2021-06-27 13:35:20 -0300

    quick fix: force R to be upper triangular

--- qr.py
+++ qr.py
@@ -2,12 +2,14 @@ import numpy as np
 
 
 def sign(x: float) -> float:
+    "Sign function: returns 1.0 if x is greater than or equal to zero and -1.0 otherwise."
     if x >= 0.0:
         return 1.0
     else:
         return -1.0
 
 def wilkinson_shift(alpha: float, beta: float, alpha_last: float) -> float:
+    "Returns "
     d = (alpha - alpha_last) / 2.0
     mu = alpha_last + d - sign(d) * np.sqrt(d**2 + beta**2)
 
@@ -36,10 +38,11 @@ def givens_matrix(n: int, i: int, j: int, c: float, s: float) -> np.ndarray:
 
     return G
 
-def givens_rotations(A):
+def givens_rotations(A: np.ndarray) -> "tuple[np.ndarray, np.ndarray]":
+    "Through Given's Rotation Method, do one step of QR decopomsition"
     n = A.shape[0]
-    R = np.copy(A)
-    Q = np.eye(n)
+    Q = np.eye(n) # Initialize ortoghonal matrix
+    R = np.copy(A) # Non-inplace operations
 
     for m in range(0, n-1, 1):
         alpha = R[m, m]
@@ -49,36 +52,42 @@ def givens_rotations(A):
         c, s = cos_and_sin(alpha, beta)
         G = givens_matrix(n, i, j, c, s)
 
+        # Find Q and R
         Q = Q @ G.T
         R = G @ R
+    
+    R = np.triu(R)
+
+    print(R)
 
     return (Q, R)
 
 def QR(A0: np.ndarray, epsilon: float=1e-6, shifted: bool=True) -> "tuple[np.ndarray, np.ndarray]":
     n = A0.shape[0]
-
-    I = np.eye(n)
-    A = np.copy(A0)
-    V = np.eye(n)
-
     k = 0
 
+    A = np.copy(A0) # Non-inplace operations
+    V = np.eye(n)   # Initialize eigenvectors matrix
+    I = np.eye(n)
+
     for m in range(n-1, 0, -1):
-        beta = A[m, m-1]
+        beta = A[m, m-1] # Element to be minimized
 
-        while np.abs(beta) >= epsilon:
+        while np.abs(beta) >= epsilon: # Loop until covergence of beta to approx. zero
             mu = wilkinson_shift(alpha, beta, alpha_last) if (shifted and k > 0) else 0.0
-            Q, R = givens_rotations(A - mu*I)
-
+            Q, R = givens_rotations(A - mu*I) # Do QR decomposition
+            
+            # Calculates new matrix
             A = R @ Q + mu*I
             V = V @ Q
-
+            
+            # Update variables
             beta = A[m, m-1]
             alpha = A[m-1, m-1]
             alpha_last = A[m, m]
 
-            k += 1
+            k += 1 # New iteration
 
-        A[m, m-1] = 0
+        A[m, m-1] = 0 # If while breaks, beta converged (considered equal to zero)
 
     return (V, A, k)
